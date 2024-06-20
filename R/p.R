7a50b5cc1c745084041214e88a2a37d7e2218173
Matias-Lopez-13 <112888781+Matias-Lopez-13@users.noreply.github.com>
2024-06-20 13:11:36 -0300

    Update p.R

--- R/p.R
+++ R/p.R
@@ -1,15 +1,15 @@
 ##' Find a p-value given opposing and supporting information
 ##' 
-##' @param num_support An integer representing the number of pieces of information in favor of the hypothesis. Must be less than or equal to the total.
-##' @param total_info An integer representing the total number of pieces of information observed
+##' @param obs_support An integer representing the number of observations in favor of the working hypothesis. Must be less than or equal to the total.
+##' @param total_obs An integer representing the total number of observations
 ##' @export
-find_p <- function(num_support,total_info){
-  ## Test to make sure that num_support is less than or equal to total_info
-  stopifnot("Number of pieces of supporting info must be less than or equal to the total number of observations"=num_support<=total_info)
-  num_oppose <- num_support+1
-  stopifnot("Number of pieces of supporting info must be greater than or equal to half of the total number of observations"=num_support >= (total_info/2))
+find_p <- function(obs_support,total_obs){
+  ## Test to make sure that obs_support is less than or equal to total_obs
+  stopifnot("The number of observations in favor of the working hypothesis must be less than or equal to the total number of observations"=obs_support<=total_obs)
+  obs_oppose <- obs_support+1
+  stopifnot("Observations are already compatible with the null. The number of observations in favor of the working hypothesis must be greater than or equal to half of the total number of observations"=obs_support >= (total_obs/2))
   ## We assume odds=1 here
- thep <- dFNCHypergeo(x=num_support, m1 = num_support, m2 = num_oppose,
-                      n = total_info, odds = 1)
+ thep <- dFNCHypergeo(x=obs_support, m1 = obs_support, m2 = obs_oppose,
+                      n = total_obs, odds = 1)
  return(thep)
 }
